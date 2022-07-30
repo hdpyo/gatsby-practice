28c891ae6df84d3d09df778162eaac4b3134190c
hdpyo <vvhooping@gmail.com>
2022-07-30 20:25:14 +0900

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import React, { FunctionComponent } from 'react';
+
+import Text from '../components/Test';
+
+const IndexPage: FunctionComponent = function () {
+  return <Text text="Home" />;
+};
+
+export default IndexPage;
